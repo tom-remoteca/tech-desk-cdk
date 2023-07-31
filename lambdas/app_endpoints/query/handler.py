960341ee280b4ff08975d26498d2a2596b4d1577
Tom Davis <rdvs.tom@gmail.com>
2023-07-31 13:11:32 +0200

    query api

--- lambdas/app_endpoints/query/handler.py
+++ lambdas/app_endpoints/query/handler.py
@@ -17,7 +17,7 @@ table = dynamodb.Table(os.environ["CORE_TABLE_NAME"])
 BUCKET_NAME = os.environ["BUCKET_NAME"]
 
 
-def response(status_code, body):
+def response(status_code, body={}):
     return {
         "statusCode": status_code,
         "headers": {
@@ -33,54 +33,38 @@ def response(status_code, body):
 def handler(event, context):
     print(event)
     # Get Query ID from Path & MEO from URLarg
-    query_id = 'lemon'
-    is_public = 'true'
+    query_id = event["pathParameters"]["query_id"]
+    is_public = event['queryStringParameters']['is_public']
+
     user_id = event['requestContext']['authorizer']['sub']
     company_id = event['requestContext']['authorizer']['tenant_id']
 
     if event["httpMethod"] == "GET":
-        return handle_get(company_id, user_id, event)
+        return handle_get(company_id, user_id, query_id=query_id, is_public=is_public)
         
     # if event["httpMethod"] == "POST":
     #     return handle_post(company_id, user_id, event)
-    return 200
+    return response(200)
 
 
 
-def handle_get(company_id, user_id, event):
-    # Define the primary key for user's own queries
-    primary_key_user = {
-        'PK': f'COMPANY#{company_id}#USER#{user_id}',
-    }
-
-    # Execute the query for user's own queries
-    response_user = table.query(
-        KeyConditionExpression=boto3.dynamodb.conditions.Key('PK').eq(primary_key_user['PK']),
-    )
-
-    # Define the primary key for public queries
-    primary_key_public = {
-        'GSI1PK': f'COMPANY#{company_id}',
-        'GSI1SK': f'PUBLIC#TRUE',
-    }
-
-    # Execute the query for public queries
-    response_public = table.query(
-        IndexName="GSI1",  # replace with your GSI name
-        KeyConditionExpression=boto3.dynamodb.conditions.Key('GSI1PK').eq(primary_key_public['GSI1PK']) & 
-                               boto3.dynamodb.conditions.Key('GSI1SK').begins_with(primary_key_public['GSI1SK']),
-        ProjectionExpression=projection_expression,
-    )
-
-    # Extract the items (queries) from the responses and de-duplicate
-    items_user = response_user['Items']
-    items_public = response_public['Items']
-
-    query_ids = set([item['SK'] for item in items_user])
-    items_public = [item for item in items_public if item['SK'] not in query_ids]
-
-    items = items_user + items_public
-
-    res = [item['query_data'] for item in items]
-
-    return response(200, res)
+def handle_get(company_id, user_id, query_id:str, is_public: bool):
+    if is_public:
+        primary_key_public = {
+            'GSI1PK': f"COMPANY#{company_id}",
+            'GSI1SK': f"PUBLIC#{is_public}QUERY#{query_id}",
+        }
+        res = table.query(
+            IndexName="GSI1",
+            KeyConditionExpression=boto3.dynamodb.conditions.Key('GSI1PK').eq(primary_key_public['GSI1PK']) & boto3.dynamodb.conditions.Key('GSI1SK').eq(primary_key_public['GSI1SK']),
+        )
+    else:
+        primary_key_private ={
+            'PK': f'COMPANY#{company_id}#USER#{user_id}',
+            'SK': f'QUERY#{query_id}'
+        }
+        res = table.query(
+            KeyConditionExpression=boto3.dynamodb.conditions.Key('PK').eq(primary_key_private['PK']) & boto3.dynamodb.conditions.Key('SK').eq(primary_key_private['SK']),
+        )
+    print(res['Item'])
+    return response(200, res['Item']['query_data'])
--- stacks/endpoints_stack.py
+++ stacks/endpoints_stack.py
@@ -34,13 +34,28 @@ class EndpointsStack(Stack):
                 "BUCKET_NAME": core_bucket.bucket_name
             },
         )
+
+        query_lambda = _lambda.Function(
+            self,
+            "QueryLambda",
+            handler="handler.handler",
+            runtime=_lambda.Runtime.PYTHON_3_9,
+            code=_lambda.Code.from_asset(
+                f"{os.path.dirname(__file__)}/../lambdas/app_endpoints/query"
+            ),
+            environment={
+                "CORE_TABLE_NAME": core_table.table_name,
+                "BUCKET_NAME": core_bucket.bucket_name
+            },
+        )
+
         core_table.grant_read_write_data(queries_lambda)
         next_auth_table.grant_read_data(queries_lambda)
         core_bucket.grant_put(queries_lambda)
 
         queries_api = api.root.add_resource("queries")
         query_api = queries_api.add_resource("{query_id}")
-        activity_api = query_api.add_resource("activity")
+        # activity_api = query_api.add_resource("activity")
 
         queries_api.add_method(
             "GET",
@@ -51,5 +66,11 @@ class EndpointsStack(Stack):
         queries_api.add_method(
             "POST",
             apigateway.LambdaIntegration(queries_lambda),
+            authorizer=api_authorizer
+        )
+
+        query_api.add_method(
+            "GET",
+            apigateway.LambdaIntegration(query_lambda),
             authorizer=api_authorizer
         )
