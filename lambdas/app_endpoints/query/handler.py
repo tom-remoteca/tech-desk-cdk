54ada10cb2701cda1b7880313230bfdbf09d18ac
Tom Davis <rdvs.tom@gmail.com>
2023-07-31 16:34:08 +0200

    still on the reformat game.

--- lambdas/app_endpoints/query/handler.py
+++ lambdas/app_endpoints/query/handler.py
@@ -1,14 +1,8 @@
 import os
-import cgi
 import json
-import uuid
 import boto3
-import time
-import base64
-from io import BytesIO
 
 from boto3.dynamodb.conditions import Key
-from botocore.exceptions import ClientError
 
 
 # Create the DynamoDB client
@@ -26,45 +20,51 @@ def response(status_code, body={}):
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
         },
-        "body": json.dumps(body, default=str)
+        "body": json.dumps(body, default=str),
     }
 
 
 def handler(event, context):
     print(event)
+
     # Get Query ID from Path & MEO from URLarg
     query_id = event["pathParameters"]["query_id"]
-    is_public = event['queryStringParameters']['is_public']
-
-    user_id = event['requestContext']['authorizer']['sub']
-    company_id = event['requestContext']['authorizer']['tenant_id']
+    is_public = event["queryStringParameters"]["is_public"]
+    user_id = event["requestContext"]["authorizer"]["sub"]
+    company_id = event["requestContext"]["authorizer"]["tenant_id"]
+    print(user_id, company_id, query_id, is_public)
 
     if event["httpMethod"] == "GET":
-        return handle_get(company_id, user_id, query_id=query_id, is_public=is_public)
-        
+        return handle_get(
+            company_id, user_id, query_id=query_id, is_public=is_public
+        )
+
     # if event["httpMethod"] == "POST":
     #     return handle_post(company_id, user_id, event)
     return response(200)
 
 
-
-def handle_get(company_id, user_id, query_id:str, is_public: bool):
+def handle_get(company_id, user_id, query_id: str, is_public: bool):
     if is_public:
         primary_key_public = {
-            'GSI1PK': f"COMPANY#{company_id}",
-            'GSI1SK': f"PUBLIC#{is_public}QUERY#{query_id}",
+            "GSI1PK": f"COMPANY#{company_id}",
+            "GSI1SK": f"PUBLIC#{is_public}QUERY#{query_id}",
         }
         res = table.query(
             IndexName="GSI1",
-            KeyConditionExpression=boto3.dynamodb.conditions.Key('GSI1PK').eq(primary_key_public['GSI1PK']) & boto3.dynamodb.conditions.Key('GSI1SK').eq(primary_key_public['GSI1SK']),
+            KeyConditionExpression=Key("GSI1PK").eq(
+                primary_key_public["GSI1PK"]
+            )
+            & Key("GSI1SK").eq(primary_key_public["GSI1SK"]),
         )
     else:
-        primary_key_private ={
-            'PK': f'COMPANY#{company_id}#USER#{user_id}',
-            'SK': f'QUERY#{query_id}'
+        primary_key_private = {
+            "PK": f"COMPANY#{company_id}#USER#{user_id}",
+            "SK": f"QUERY#{query_id}",
         }
         res = table.query(
-            KeyConditionExpression=boto3.dynamodb.conditions.Key('PK').eq(primary_key_private['PK']) & boto3.dynamodb.conditions.Key('SK').eq(primary_key_private['SK']),
+            KeyConditionExpression=Key("PK").eq(primary_key_private["PK"])
+            & Key("SK").eq(primary_key_private["SK"]),
         )
-    print(res['Item'])
-    return response(200, res['Item']['query_data'])
+    print(res["Item"])
+    return response(200, res["Item"]["query_data"])
