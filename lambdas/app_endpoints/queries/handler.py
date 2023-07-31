2ff0dbd247d75057d9320d9495a0dea4c170fbb5
Tom Davis <rdvs.tom@gmail.com>
2023-07-31 15:48:35 +0200

    blacked.

--- lambdas/app_endpoints/queries/handler.py
+++ lambdas/app_endpoints/queries/handler.py
@@ -8,7 +8,6 @@ import base64
 from io import BytesIO
 
 from boto3.dynamodb.conditions import Key
-from botocore.exceptions import ClientError
 
 
 # Create the DynamoDB client
@@ -17,38 +16,46 @@ table = dynamodb.Table(os.environ["CORE_TABLE_NAME"])
 
 BUCKET_NAME = os.environ["BUCKET_NAME"]
 
+
 def write_file_to_s3(query_id, file_data):
     # print(file_data)
     # Initialize Boto3 S3 client
-    s3_client = boto3.client('s3')
+    s3_client = boto3.client("s3")
 
     attachment_id = f"attachment_{uuid.uuid4()}"
 
     file_key = f"queries/{query_id}/{attachment_id}"
 
     # Write the file to S3 bucket
-    s3_client.upload_fileobj(file_data['file_data_bytes'], BUCKET_NAME, file_key, ExtraArgs={
-                             'ContentType': file_data['content_type']})
+    s3_client.upload_fileobj(
+        file_data["file_data_bytes"],
+        BUCKET_NAME,
+        file_key,
+        ExtraArgs={"ContentType": file_data["content_type"]},
+    )
     return attachment_id, file_key
-    # s3_client.put_object(Bucket=BUCKET_NAME, Key=f'2-{file_key}', Body=file_data['file_data_bytes'])
 
 
 def parse_raw_query(query_id, event):
     # Decode base64 encoded data from the event
-    encoded_data = event['body']
+    encoded_data = event["body"]
     decoded_data = base64.b64decode(encoded_data)
 
     # Parse the content type and boundary from the 'Content-Type' header
-    content_type_header = event['headers']['content-type']
+    content_type_header = event["headers"]["content-type"]
     content_type, options = cgi.parse_header(content_type_header)
-    boundary = options.get('boundary')
+    # boundary = options.get("boundary")
 
     # Create a BytesIO object to use as the file pointer
     fp = BytesIO(decoded_data)
 
     # Create a FieldStorage object to parse the multipart form data
-    fs = cgi.FieldStorage(fp=fp, headers=event['headers'], environ={
-                          'REQUEST_METHOD': 'POST'}, keep_blank_values=True)
+    fs = cgi.FieldStorage(
+        fp=fp,
+        headers=event["headers"],
+        environ={"REQUEST_METHOD": "POST"},
+        keep_blank_values=True,
+    )
 
     # Dictionary to store the parsed form data
     form_data = {}
@@ -57,10 +64,10 @@ def parse_raw_query(query_id, event):
     for field in fs.list:
         if field.filename:  # This is a file field
             form_data[field.name] = {
-                'file_name': field.filename,
-                'content_type': field.type,
-                'file_data': field.file,  # Read the file data here
-                'file_data_bytes': BytesIO(field.file.read())
+                "file_name": field.filename,
+                "content_type": field.type,
+                "file_data": field.file,  # Read the file data here
+                "file_data_bytes": BytesIO(field.file.read()),
             }
         else:  # This is a regular form field
             form_data[field.name] = field.value
@@ -68,50 +75,54 @@ def parse_raw_query(query_id, event):
 
     out = {}
     attachments = []
-    out['query_id'] = query_id
+    out["query_id"] = query_id
     for k, v in form_data.items():
         if k.startswith("file-"):
             attachment_id, file_key = write_file_to_s3(
-                query_id=query_id, file_data=v)
-            attachments.append({
-                "file_name": v['file_name'],
-                "attachment_id": attachment_id,
-                "file_key": file_key
-            })
+                query_id=query_id, file_data=v
+            )
+            attachments.append(
+                {
+                    "file_name": v["file_name"],
+                    "attachment_id": attachment_id,
+                    "file_key": file_key,
+                }
+            )
             continue
         elif k == "usecase_tags":
             raw_tags = json.loads(v)
-            tags = [obj['value'] for obj in raw_tags]
-            out['usecase_tags'] = tags
+            tags = [obj["value"] for obj in raw_tags]
+            out["usecase_tags"] = tags
         else:
             out[k] = v
-    out['attachments'] = attachments
+    out["attachments"] = attachments
     return out
 
 
 def create_query_dynamo(company_id, user_id, query_id, query_data):
     print(query_data)
-    if query_data['is_public'] == "true":
+    if query_data["is_public"] == "true":
         is_public = "TRUE"
     else:
         is_public = "FALSE"
-        
-    
-    query_data['id'] = query_id
-    query_data['date_submitted'] = str(int(time.time()))
-    query_data['query_status'] = "Submitted"
-    query_data['company_id'] = company_id
 
-    res = table.put_item(
+    query_data["id"] = query_id
+    query_data["date_submitted"] = str(int(time.time()))
+    query_data["query_status"] = "Submitted"
+    query_data["company_id"] = company_id
+
+    table.put_item(
         Item={
-            'PK': f"COMPANY#{company_id}#USER#{user_id}",
-            'SK': f"QUERY#{query_id}",
-            'GSI1PK': f"COMPANY#{company_id}",
-            'GSI1SK': f"PUBLIC#{is_public}QUERY#{query_id}",
-            'query_data': query_data
+            "PK": f"COMPANY#{company_id}#USER#{user_id}",
+            "SK": f"QUERY#{query_id}",
+            "GSI1PK": f"COMPANY#{company_id}",
+            "GSI1SK": f"PUBLIC#{is_public}QUERY#{query_id}",
+            "query_data": query_data,
         }
     )
+
     return query_data
+
 
 def response(status_code, body):
     return {
@@ -122,18 +133,18 @@ def response(status_code, body):
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
         },
-        "body": json.dumps(body, default=str)
+        "body": json.dumps(body, default=str),
     }
 
 
 def handler(event, context):
     print(event)
-    user_id = event['requestContext']['authorizer']['sub']
-    company_id = event['requestContext']['authorizer']['tenant_id']
+    user_id = event["requestContext"]["authorizer"]["sub"]
+    company_id = event["requestContext"]["authorizer"]["tenant_id"]
     print(user_id)
     if event["httpMethod"] == "GET":
         return handle_get(company_id, user_id, event)
-        
+
     if event["httpMethod"] == "POST":
         return handle_post(company_id, user_id, event)
     return 200
@@ -145,7 +156,7 @@ def handle_post(company_id, user_id, event):
 
     # Process data in webkit form and save attachments to s3
     parsed_query = parse_raw_query(query_id, event)
-    
+
     # Save this request to Dynamo
     create_query_dynamo(company_id, user_id, query_id, parsed_query)
 
@@ -155,41 +166,44 @@ def handle_post(company_id, user_id, event):
 def handle_get(company_id, user_id, event):
     # Define the primary key for user's own queries
     primary_key_user = {
-        'PK': f'COMPANY#{company_id}#USER#{user_id}',
+        "PK": f"COMPANY#{company_id}#USER#{user_id}",
     }
 
-    projection_expression = 'SK, query_data.id, query_data.is_public, query_data.query_title, query_data.query_status, query_data.date_submitted'
+    projection_expression = "SK, query_data.id, query_data.is_public, \
+                            query_data.query_title, query_data.query_status, \
+                            query_data.date_submitted"
 
     # Execute the query for user's own queries
     response_user = table.query(
-        KeyConditionExpression=boto3.dynamodb.conditions.Key('PK').eq(primary_key_user['PK']),
+        KeyConditionExpression=Key("PK").eq(primary_key_user["PK"]),
         ProjectionExpression=projection_expression,
-
     )
 
     # Define the primary key for public queries
     primary_key_public = {
-        'GSI1PK': f'COMPANY#{company_id}',
-        'GSI1SK': f'PUBLIC#TRUE',
+        "GSI1PK": f"COMPANY#{company_id}",
+        "GSI1SK": "PUBLIC#TRUE",
     }
 
     # Execute the query for public queries
     response_public = table.query(
         IndexName="GSI1",  # replace with your GSI name
-        KeyConditionExpression=boto3.dynamodb.conditions.Key('GSI1PK').eq(primary_key_public['GSI1PK']) & 
-                               boto3.dynamodb.conditions.Key('GSI1SK').begins_with(primary_key_public['GSI1SK']),
+        KeyConditionExpression=Key("GSI1PK").eq(primary_key_public["GSI1PK"])
+        & Key("GSI1SK").begins_with(primary_key_public["GSI1SK"]),
         ProjectionExpression=projection_expression,
     )
 
     # Extract the items (queries) from the responses and de-duplicate
-    items_user = response_user['Items']
-    items_public = response_public['Items']
+    items_user = response_user["Items"]
+    items_public = response_public["Items"]
 
-    query_ids = set([item['SK'] for item in items_user])
-    items_public = [item for item in items_public if item['SK'] not in query_ids]
+    query_ids = set([item["SK"] for item in items_user])
+    items_public = [
+        item for item in items_public if item["SK"] not in query_ids
+    ]
 
     items = items_user + items_public
 
-    res = [item['query_data'] for item in items]
+    res = [item["query_data"] for item in items]
 
     return response(200, res)
--- stacks/core_stack.py
+++ stacks/core_stack.py
@@ -1,18 +1,7 @@
 from aws_cdk import aws_s3 as s3
 from aws_cdk import aws_iam as iam
-from aws_cdk import aws_lambda as _lambda
-from aws_cdk import aws_route53 as route53
 from aws_cdk import aws_dynamodb as dynamodb
-from aws_cdk import aws_apigateway as apigateway
-from aws_cdk import aws_certificatemanager as acm
-from aws_cdk import aws_route53_targets as targets
-
-from constructs import Construct
-from aws_cdk import CfnOutput, Stack, SecretValue
-
-import aws_cdk.aws_amplify_alpha as amplify
-
-import os
+from aws_cdk import CfnOutput, Stack
 
 
 class CoreStack(Stack):
@@ -24,7 +13,6 @@ class CoreStack(Stack):
         # user_pool,
         **kwargs,
     ) -> None:
-
         super().__init__(scope, id, **kwargs)
 
         bucket = s3.Bucket(
@@ -42,19 +30,18 @@ class CoreStack(Stack):
                 name="PK", type=dynamodb.AttributeType.STRING
             ),
             sort_key=dynamodb.Attribute(
-                name="SK", type=dynamodb.AttributeType.STRING),
+                name="SK", type=dynamodb.AttributeType.STRING
+            ),
             billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST,
         )
         table.add_global_secondary_index(
             index_name="GSI1",
             partition_key=dynamodb.Attribute(
-                name="GSI1PK",
-                type=dynamodb.AttributeType.STRING
+                name="GSI1PK", type=dynamodb.AttributeType.STRING
             ),
             sort_key=dynamodb.Attribute(
-                name="GSI1SK",
-                type=dynamodb.AttributeType.STRING
-            )
+                name="GSI1SK", type=dynamodb.AttributeType.STRING
+            ),
         )
         self.table = table
 
@@ -75,60 +62,48 @@ class CoreStack(Stack):
             id="NextAuthTable",
             table_name="next-auth",
             partition_key=dynamodb.Attribute(
-                name="pk",
-                type=dynamodb.AttributeType.STRING
+                name="pk", type=dynamodb.AttributeType.STRING
             ),
             sort_key=dynamodb.Attribute(
-                name="sk",
-                type=dynamodb.AttributeType.STRING
+                name="sk", type=dynamodb.AttributeType.STRING
             ),
-            time_to_live_attribute="expires"
+            time_to_live_attribute="expires",
         )
         # Add a global secondary index to the table
         next_auth_table.add_global_secondary_index(
             index_name="GSI1",
             partition_key=dynamodb.Attribute(
-                name="GSI1PK",
-                type=dynamodb.AttributeType.STRING
+                name="GSI1PK", type=dynamodb.AttributeType.STRING
             ),
             sort_key=dynamodb.Attribute(
-                name="GSI1SK",
-                type=dynamodb.AttributeType.STRING
-            )
+                name="GSI1SK", type=dynamodb.AttributeType.STRING
+            ),
         )
         self.next_auth_table = next_auth_table
         # Output the access key details
-        CfnOutput(
-            self, "NextAuthTableName",
-            value=next_auth_table.table_name
-        )
+        CfnOutput(self, "NextAuthTableName", value=next_auth_table.table_name)
         # Create an IAM user
         next_auth_user = iam.User(
-            self, "NextAuthUser2",
-            user_name="next-auth-user-2"
+            self, "NextAuthUser2", user_name="next-auth-user-2"
         )
 
         # Attach DynamoDB read/write permissions to the user
         dynamodb_policy = iam.PolicyStatement(
-            actions=["dynamodb:*"],
-            resources=[next_auth_table.table_arn]
+            actions=["dynamodb:*"], resources=[next_auth_table.table_arn]
         )
         next_auth_user.add_to_policy(dynamodb_policy)
 
         # Create an access key for the user
         access_key = iam.CfnAccessKey(
-            self, "NextAuthUserAccessKey",
-            user_name=next_auth_user.user_name
+            self, "NextAuthUserAccessKey", user_name=next_auth_user.user_name
         )
 
         # Output the access key details
+        CfnOutput(self, "NextAuthUserAccessKeyId", value=access_key.ref)
         CfnOutput(
-            self, "NextAuthUserAccessKeyId",
-            value=access_key.ref
-        )
-        CfnOutput(
-            self, "NextAuthUserSecretAccessKey",
-            value=access_key.attr_secret_access_key
+            self,
+            "NextAuthUserSecretAccessKey",
+            value=access_key.attr_secret_access_key,
         )
 
         ####### Amplify ######
